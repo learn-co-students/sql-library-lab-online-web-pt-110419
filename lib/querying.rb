9080b39377c811d93d36f9620e1abce41ec639c7
Antony Sanders <antony.devon92@gmail.com>
2020-01-24 11:19:51 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters ORDER BY motto LIMIT 1;"
 end
 
 
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors INNER JOIN subgenres ON "
 end
 
 def select_series_title_with_most_human_characters
