bb17a694faeb918cb79235df9592bf4e370e3003
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-02 01:38:41 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,5 +10,19 @@ INSERT INTO authors(name)
               VAULES  ("piers anthony"),
                       ("iain pear");
                       
-INSERT INTO 
+INSERT INTO books(title, year, series_id)
+              VALUES ("split infinity", 1999, 1)
+                     ("blue adept", 1999, 1)
+                     ("out of phaze", 1999, 1)
+                     ("the notebook", 1999, 2)
+                     ("message in a bottle", 1999, 2)
+                     ("when harry met sally", 1999, 2);
+
+
+
+
+
+
+
+
               
