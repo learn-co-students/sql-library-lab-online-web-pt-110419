3666961415cd46f09530444061513981d1a6a267
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-02 01:31:52 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,14 @@
-INSERT INTO SERIES(title, author_id, subgenre_id)
-            VALUES("movies", 1, 1),
-                  ("books", 2, 2);
+INSERT INTO series(title, author_id, subgenre_id)
+            VALUES  ("movies", 1, 1),
+                    ("books", 2, 2);
+                  
+INSERT INTO subgenres(name)
+              VALUES  ("mystery"),
+                      ("romance");
+                      
+INSERT INTO authors(name)
+              VAULES  ("piers anthony"),
+                      ("iain pear");
+                      
+INSERT INTO 
+              
