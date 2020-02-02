6dcd4814fdaf42c7a63b3c5da3a3c342bc6e32fa
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-02 05:55:15 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,29 +1,11 @@
-INSERT INTO series (title, author_id, subgenre_id)
-            VALUES('A', 1,1), 
-                  ('B', 1,1);
-                    
+INSERT INTO series(title, author_id, subgenre_id) VALUES("abc",1,1), ("def",2,2);
+
                   
-INSERT INTO subgenres(name)
-              VALUES  ("mystery"),
-                      ("romance");
-                      
-INSERT INTO authors(name)
-              VAULES  ("piers anthony"),
-                      ("iain pear");
+
                       
 INSERT INTO books(title, year, series_id)
-              VALUES ("split infinity", 1999, 1)
-                     ("blue adept", 1999, 1)
-                     ("out of phaze", 1999, 1)
-                     ("the notebook", 1999, 2)
-                     ("message in a bottle", 1999, 2)
+              VALUES ("split infinity", 1999, 1), ("blue adept", 1999, 1),
+                     ("out of phaze", 1999, 1),
+                     ("the notebook", 1999, 2),
+                     ("message in a bottle", 1999, 2),
                      ("when harry met sally", 1999, 2);
-
-
-
-
-
-
-
-
-              
