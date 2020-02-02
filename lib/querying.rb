40de2f5400d95c1e99309f2f3ed24c52c29d98c2
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-02 06:44:39 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, species.count From characters WHERE MAX(species.count)"
+  "SELECT species, COUNT(species) From characters WHERE species = 'human'"
 end
 
 def select_name_and_series_subgenres_of_authors
